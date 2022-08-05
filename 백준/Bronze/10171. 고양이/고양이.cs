23bc35e1f948a37d4144f385f5fd6609c9fc1e74
JeongJuChan <95285906+JeongJuChan@users.noreply.github.com>
2022-08-05 09:36:48 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BlackJackProject
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
