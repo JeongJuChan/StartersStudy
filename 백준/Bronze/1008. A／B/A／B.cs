77dd4431bc4783cbafbfbbdcbcad418f1df537ac
JeongJuChan <95285906+JeongJuChan@users.noreply.github.com>
2022-08-05 10:10:12 +0900

    [Bronze V] Title: A/B, Time: 64 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BlackJackProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] num = new int[2];
+            for (int i = 0; i < input.Length; i++)
+            {
+                num[i]= int.Parse(input[i]);
+            }
+            Console.WriteLine((double)num[0] / num[1]);
+        }
+    }
+}
