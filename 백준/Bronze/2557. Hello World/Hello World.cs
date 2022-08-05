618f838103e7f8c51b4cd6e39990a9611f24780c
JeongJuChan <95285906+JeongJuChan@users.noreply.github.com>
2022-08-05 09:20:34 +0900

    [Bronze V] Title: Hello World, Time: 60 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BlackJackProject
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
