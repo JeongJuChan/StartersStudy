674219b764bd09fa35737a2c3b92851113e511fa
JeongJuChan <95285906+JeongJuChan@users.noreply.github.com>
2022-08-05 10:18:12 +0900

    [Bronze V] Title: ??!, Time: 80 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BlackJackProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input + "??!");
+        }
+    }
+}
