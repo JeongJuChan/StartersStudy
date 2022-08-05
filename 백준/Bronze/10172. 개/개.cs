6a3877d5482ff59f2d00a17e698625059c234cad
JeongJuChan <95285906+JeongJuChan@users.noreply.github.com>
2022-08-05 09:50:02 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BlackJackProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"'    |");
+            Console.WriteLine("||_/=\\\\__|");*/
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+        }
+    }
+}
