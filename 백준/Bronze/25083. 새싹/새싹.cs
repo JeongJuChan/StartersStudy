a3c08a82655862ba75f2a15df07cbb4ce8ef7360
JeongJuChan <95285906+JeongJuChan@users.noreply.github.com>
2022-08-05 14:04:12 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BlackJackProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r' ");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
