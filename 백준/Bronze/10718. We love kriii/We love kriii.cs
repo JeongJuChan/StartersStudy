a897b5181d01538a364da9cf2e9ac55fa17b6456
JeongJuChan <95285906+JeongJuChan@users.noreply.github.com>
2022-08-05 09:19:38 +0900

    [Bronze V] Title: We love kriii, Time: 72 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BlackJackProject
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("강한친구 대한육군");
+            }
+        }
+    }
+}
